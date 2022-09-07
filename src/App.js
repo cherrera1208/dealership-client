1f5f7c02e49d8d0790ad2c08cf31115ad7c64410
David Suy <DavidSuyDev@gmail.com>
2022-09-06 18:33:40 -0700

    add carousel

--- src/App.js
+++ src/App.js
@@ -2,14 +2,41 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import axios from 'axios';
+
+const SERVER = process.env.REACT_APP_SERVER;
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      featuredCars: [],
+    };
+  }
+
+  getCars = async (req, res) => {
+    try {
+      let result = await axios.get(`${SERVER}/cars`);
+      this.setState({
+        featuredCars: result.data,
+      });
+
+      return result;
+    } catch (error) {
+      console.log('we have an error: ', error.response.data);
+    }
+  };
+
+  componentDidMount() {
+    this.getCars();
+  }
+
   render() {
     return (
       <>
-        <h1>Purple dealership</h1>;
+        <h1>Purple dealership</h1>
         <Header />
-        <Main />
+        <Main featuredCars={this.state.featuredCars} />
         <Footer />
       </>
     );
